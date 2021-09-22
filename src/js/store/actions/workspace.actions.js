6c8f8c6749df834d549f4e470172beb31b445b71
On Chetrit <on7chetrit@gmail.com>
2021-09-23 00:20:05 +0300

    workspace actions

--- src/js/store/actions/workspace.actions.js
+++ src/js/store/actions/workspace.actions.js
@@ -14,16 +14,60 @@ export function loadWorkspaces() {
     }
 }
 
-export function loadWorkspace() {
+export function loadWorkspace(workspace) {
     return async dispatch => {
         try {
-            const workspace = await workspaceService.query()
             dispatch({
-                type: 'SET_WORKSPACES',
+                type: 'SET_WORKSPACE',
                 workspace
             })
         } catch (err) {
             console.log('Cannot load workspaces', err)
+        }
+    }
+}
+
+export function addWorkspace(workspace) {
+    return async dispatch => {
+        try {
+            const addedWorkspace = workspaceService.save(workspace)
+            dispatch({
+                type: 'ADD_WORKSPACE',
+                addedWorkspace
+            })
+        } catch (err) {
+            console.log('Cannot load workspaces', err)
+        }
+    }
+}
+
+export function editWorkspace(workspaceToSave) {
+    return async dispatch => {
+        try {
+            const savedWorkspace = await workspaceService.save(workspaceToSave)
+            console.log('Updated Workspace:', savedWorkspace);
+            dispatch({
+                type: 'EDIT_WORKSPACE',
+                workspace: savedWorkspace
+            })
+        }
+        catch (err) {
+            console.log('Cannot save workspace', err)
+        }
+    }
+}
+
+export function removeWorkspace(workspaceId) {
+    return async dispatch => {
+        try {
+            await workspaceService.remove(workspaceId)
+            dispatch({
+                type: 'REMOVE_WORKSPACE',
+                workspaceId
+            })
+        }
+        catch (err) {
+            console.log('Cannot remove workspace', err)
         }
     }
 }
