fff6edba4db5d9ce625e284849f06b4677fdbec2
On Chetrit <on7chetrit@gmail.com>
2021-09-23 10:15:26 +0300

    boardDetails

--- src/js/cmps/WorkspaceNav.jsx
+++ src/js/cmps/WorkspaceNav.jsx
@@ -1,21 +1,23 @@
 import { Component } from 'react';
 import { connect } from 'react-redux';
 import { BoardList } from './board/BoardList';
-import { loadWorkspaces,loadWorkspace } from '../store/actions/workspace.actions';
+import {
+  loadWorkspaces,
+  loadWorkspace,
+} from '../store/actions/workspace.actions';
 
 class _WorkspaceNav extends Component {
-state={
-}
+  state = {};
 
   componentDidMount() {
-    this.props.loadWorkspaces()
+    this.props.loadWorkspaces();
     this.props.loadWorkspace(this.props.workspaces[0]);
   }
 
   render() {
-    const { workspaces,workspace } = this.props;
-    // console.log(`workspace`, workspace)
-    if(!workspaces.length || !workspace ) return <div>loading</div>
+    const { workspaces, workspace } = this.props;
+    console.log(`workspace`, workspace);
+    if (!workspaces.length || !workspace) return <div>loading</div>;
     return (
       <div className="workspace-nav">
         <select name="" id="">
@@ -24,7 +26,6 @@ state={
               <option key={workspace._id} value={workspace.name}>
                 {workspace.name}
               </option>
-              
             );
           })}
         </select>
@@ -34,7 +35,7 @@ state={
         <button>Search</button>
 
         <div className="">
-          <BoardList workspace={workspace}/>
+          <BoardList workspace={workspace} />
         </div>
       </div>
     );
@@ -50,7 +51,7 @@ function mapStateToProps(state) {
 
 const mapDispatchToProps = {
   loadWorkspaces,
-  loadWorkspace
+  loadWorkspace,
 };
 export const WorkspaceNav = connect(
   mapStateToProps,
--- src/js/cmps/board/BoardDetails.jsx
+++ src/js/cmps/board/BoardDetails.jsx
@@ -1,47 +1,46 @@
-import { Component } from "react";
+import { Component } from 'react';
 import { connect } from 'react-redux';
 
 import { loadBoard } from '../../store/actions/board.actions';
-import { WorkspaceNav } from "../WorkspaceNav";
-import { GroupPreview } from "./GroupPreview";
+import { WorkspaceNav } from '../WorkspaceNav';
+import { GroupPreview } from './GroupPreview';
 
 class _BoardDetails extends Component {
-    componentDidMount() {
-        const { boardId } = this.props.match.params;
-        this.props.loadBoard(this.props.workspace,boardId)
-    }
-    componentDidUpdate(){
-        const { boardId } = this.props.match.params;
-        this.props.loadBoard(this.props.workspace,boardId)
+  componentDidMount() {
+    const { boardId } = this.props.match.params;
+    this.props.loadBoard(this.props.workspace, boardId);
+  }
+  componentDidUpdate() {
+    const { boardId } = this.props.match.params;
+    this.props.loadBoard(this.props.workspace, boardId);
+  }
 
-    }
-
-    render() {
-        const { board} = this.props
-        console.log(board);
-        if(!board) return <div className="">loading</div>
-        return (
-            <div className="groups-container">
-                <WorkspaceNav />
-            {board.groups.map(group=>{
-                return <GroupPreview key={group.id} group={group} />
-            })}
-            </div>
-        )
-    }
+  render() {
+    const { board } = this.props;
+    console.log(board);
+    if (!board) return <div className="">loading</div>;
+    return (
+      <div className="groups-container">
+        <WorkspaceNav />
+        {board.groups.map((group) => {
+          return <GroupPreview key={group.id} group={group} />;
+        })}
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-    return {
-        board: state.boardModule.board,
-        workspace:state.workspaceModule.workspace,
-    };
+  return {
+    board: state.boardModule.board,
+    workspace: state.workspaceModule.workspace,
+  };
 }
 
 const mapDispatchToProps = {
-    loadBoard,
+  loadBoard,
 };
 export const BoardDetails = connect(
-    mapStateToProps,
-    mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(_BoardDetails);
