c580065479639566684726defae052ca63695dd4
On Chetrit <on7chetrit@gmail.com>
2021-09-23 14:45:04 +0300

    pushh

--- src/js/cmps/SidebarHeader.jsx
+++ src/js/cmps/SidebarHeader.jsx
@@ -1,9 +1,23 @@
 import { WorkspaceNav } from './WorkspaceNav';
+import Workspace from 'monday-ui-react-core/dist/icons/Workspace';
+import Notifications from 'monday-ui-react-core/dist/icons/Notifications';
+import Inbox from 'monday-ui-react-core/dist/icons/Inbox';
+import MyWeek from 'monday-ui-react-core/dist/icons/MyWeek';
+import Invite from 'monday-ui-react-core/dist/icons/Invite';
+import Help from 'monday-ui-react-core/dist/icons/Help';
+// import { Button } from 'monday-ui-react-core';
 
 export function SidebarHeader() {
   return (
     <div className="sidebar-container">
-      <nav></nav>
+      <nav>
+        <Workspace />
+        <Notifications />
+        <Inbox />
+        <MyWeek />
+        <Invite />
+        <Help />
+      </nav>
       <p>hello from main sidenav</p>
       <WorkspaceNav />
     </div>
--- src/js/cmps/board/BoardActions.jsx
+++ src/js/cmps/board/BoardActions.jsx
@@ -1,9 +1,14 @@
+import Filter from 'monday-ui-react-core/dist/icons/Filter';
+import Person from 'monday-ui-react-core/dist/icons/Person';
+import Sort from 'monday-ui-react-core/dist/icons/Sort';
 export function BoardActions() {
-    return (
-        <div>
-            <h3>hello from Actions</h3>
-
-
-        </div>
-    )
+  return (
+    <div>
+      <h3>
+        <Filter />
+        <Person />
+        <Sort />
+      </h3>
+    </div>
+  );
 }
