9578f3f383672a1e13da5384a1d1dfa49ad2ff1d
Ron Buchris <ronbuchris@gmail.com>
2021-09-30 21:14:53 +0300

    date picker

--- src/js/services/async-storage.service.js
+++ src/js/services/async-storage.service.js
@@ -727,8 +727,8 @@ const gUsers = [
     },
 ]
 
-_save('workspaceDB', gWorkspaces)
-_save('userDB', gUsers)
+// _save('workspaceDB', gWorkspaces)
+// _save('userDB', gUsers)
 
 
 function query(entityType, delay = 400) {
