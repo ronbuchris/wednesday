3295456aa3eeed65cb81ea9ad25d551b05626012
Adir cohen <adirco123@gmail.com>
2021-09-30 12:24:52 +0300

    dsa

--- src/js/cmps/board/BoardHeader.jsx
+++ src/js/cmps/board/BoardHeader.jsx
@@ -1,8 +1,17 @@
 // import React from 'react';
+import React from 'react';
+
+import { connect } from 'react-redux';
+
 import { BoardActions } from './BoardActions';
 import Menu from 'monday-ui-react-core/dist/icons/Menu';
+import Table from 'monday-ui-react-core/dist/icons/Table';
+import Dashboard from 'monday-ui-react-core/dist/icons/Dashboard';
+import Favorite from 'monday-ui-react-core/dist/icons/Favorite';
+import Info from 'monday-ui-react-core/dist/icons/Info';
+import { BsKanban } from 'react-icons/bs';
 
-export function BoardHeader({ board, onBlur, onAddItem, onEditGroup, onRemoveBoard, changeView }) {
+function _BoardHeader({ board, onBlur, onAddItem, onEditGroup, changeView, isViewChange }) {
   return (
     <div className="board-header flex column">
       <div className="board-header-top flex align-center">
@@ -14,7 +23,7 @@ export function BoardHeader({ board, onBlur, onAddItem, onEditGroup, onRemoveBoa
             onBlur(ev.target.innerText, board.title, board, 'board');
           }}
         >
-          <h1>{board.title}</h1>
+          <h1>{board.title}<Info /><Favorite /></h1>
         </div>
         <div className="menu-btn header-btn flex align-center justify-center btn br4">
           <Menu />
@@ -31,15 +40,16 @@ export function BoardHeader({ board, onBlur, onAddItem, onEditGroup, onRemoveBoa
       >
         {board.description}
       </div>
-      <div>
-      <button onClick={(ev) => {
-        ev.preventDefault();
+      <div className="view-container flex align-center">
+        <div className={`flex align-center table-view btn ${!isViewChange ?'active':'' }`} onClick={(ev) => {
+          ev.preventDefault();
           changeView(false)
-      }}>Main Table</button>
-        <button onClick={(ev) => {
+        }}><Table />Main Table</div>
+        <div className={`flex align-center table-view btn ${isViewChange ? 'active':''}`} onClick={(ev) => {
           ev.preventDefault();
           changeView(true)
-        }}>Dashboard</button>
+        }}><Dashboard />Dashboard</div>
+        <div className='flex align-center table-view btn'><BsKanban />Kanban</div>
       </div>
       <div className="divider"></div>
       <BoardActions
@@ -50,3 +60,17 @@ export function BoardHeader({ board, onBlur, onAddItem, onEditGroup, onRemoveBoa
     </div>
   );
 }
+
+function mapStateToProps(state) {
+  return {
+    isViewChange: state.boardModule.isViewChange,
+  };
+}
+
+const mapDispatchToProps = {
+
+};
+export const BoardHeader = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(_BoardHeader);
