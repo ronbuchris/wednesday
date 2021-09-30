7cc408e4c4f3d40a6c862243089bd5b65d041c0b
Adir cohen <adirco123@gmail.com>
2021-09-30 17:53:32 +0300

    fix filter

--- src/js/cmps/item/StatusFilter.jsx
+++ src/js/cmps/item/StatusFilter.jsx
@@ -2,9 +2,12 @@
 
 
 export function StatusFilter({ board, onFilterStatus }) {
+    const statusIdx = () => {
+        return board.columns.findIndex((column) => column.type === 'status');
+    };
     return (
         <div>
-            {board.columns[1].labels.map(label => {
+            {board.columns[statusIdx()].labels.map(label => {
                 return <div key={label.color} onClick={() => {
                     onFilterStatus(label.title)
                 }}>
