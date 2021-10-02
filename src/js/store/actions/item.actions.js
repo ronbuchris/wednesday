9a0ec7fbc6649eac83fce7f2a8f0ddba6142d4e2
Adir cohen <adirco123@gmail.com>
2021-10-02 16:40:10 +0300

    fhf

--- src/js/store/actions/item.actions.js
+++ src/js/store/actions/item.actions.js
@@ -107,7 +107,6 @@ export function removeItem(workspace, group, itemId) {
 }
 
 export function saveItem(item, user, workspace, group, addToTop, board, Duplicate) {
-    console.log('Duplicate',Duplicate);
     return async (dispatch) => {
         try {
             const newWorkspace = itemService.save(item, group, workspace, user, addToTop, board, Duplicate)
