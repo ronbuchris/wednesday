6b0e739c1836d99b0ee1c038cecc8f64afbac3b3
On Chetrit <on7chetrit@gmail.com>
2021-10-07 13:48:10 +0300

    cls

--- api/workspace/workspace.routes.js
+++ api/workspace/workspace.routes.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const { requireAuth} = require('../../middlewares/requireAuth.middleware')
+const { requireAuth } = require('../../middlewares/requireAuth.middleware')
 const { log } = require('../../middlewares/logger.middleware')
-const { getWorkspaces, getWorkspaceById, addWorkspace, updateWorkspace, removeWorkspace,getLabels, addReview } = require('./workspace.controller')
+const { getWorkspaces, getWorkspaceById, addWorkspace, updateWorkspace, removeWorkspace, getLabels, addReview } = require('./workspace.controller')
 const router = express.Router()
 
 // middleware that is specific to this router
@@ -10,7 +10,8 @@ const router = express.Router()
 router.get('/', log, getWorkspaces)
 router.get('/:id', getWorkspaceById)
 router.post('/', requireAuth, addWorkspace)
-router.put('/:id', requireAuth, updateWorkspace)
-router.delete('/:id', requireAuth,removeWorkspace)
+router.put('/:id', updateWorkspace)
+// router.put('/:id', requireAuth, updateWorkspace)
+router.delete('/:id', requireAuth, removeWorkspace)
 
 module.exports = router
