20ba27eae83e66d087a61a041ac3a39961a8aeb6
On Chetrit <on7chetrit@gmail.com>
2021-10-08 14:24:48 +0300

    return

--- api/user/user.service.js
+++ api/user/user.service.js
@@ -21,6 +21,8 @@ async function query(filterBy = {}) {
         users = users.map(user => {
             delete user.password
             user.createdAt = ObjectId(user._id).getTimestamp()
+            // Returning fake fresh data
+            // user.createdAt = Date.now() - (1000 * 60 * 60 * 24 * 3) // 3 days ago
             return user
         })
 
