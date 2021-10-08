394daa6784161a5242ef238a89f51fd048753bd7
Ron Buchris <ronbuchris@gmail.com>
2021-10-08 16:33:56 +0300

    a

--- server.js
+++ server.js
@@ -49,9 +49,9 @@ connectSockets(http, session)
 // Make every server-side-route to match the index.html
 // so when requesting http://localhost:3030/index.html/workspace/123 it will still respond with
 // our SPA (single page app) (the index.html file) and allow vue-router to take it from there
-// app.get('/**', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'))
-// })
+app.get('/**', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 const logger = require('./services/logger.service')
 const port = process.env.PORT || 3030
