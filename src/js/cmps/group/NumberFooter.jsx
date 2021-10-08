0a50e5d747f385c90bebb2497e32087e0a43c808
On Chetrit <on7chetrit@gmail.com>
2021-10-08 22:01:20 +0300

    number footer

--- src/js/cmps/group/NumberFooter.jsx
+++ src/js/cmps/group/NumberFooter.jsx
@@ -1,18 +1,55 @@
 export function NumberFooter({
   toggleMenus,
   toggleMenu,
+  columnIdx,
   column,
   group,
   board,
 }) {
+  const calcNumbers = (func) => {
+    console.log(`func`, func);
+    const numbers = group.items.filter(
+      (item) => item.columns[columnIdx]?.number
+    );
+    if (!numbers.length) return 0;
+    console.log(`numbers`, numbers);
+    switch (func) {
+      case 'avg':
+        return (
+          numbers.reduce((acc, num) => acc + num.columns[columnIdx].number) /
+          numbers.length
+        );
+
+      case 'sum':
+        return numbers.reduce(
+          (acc, num) => acc + num.columns[columnIdx]?.number
+        );
+
+      case 'min':
+        return Math.min(...numbers);
+
+      case 'max':
+        return Math.max(...numbers);
+
+      case 'count':
+        return numbers.length;
+
+      default:
+        break;
+    }
+  };
+
   return (
     <div
       style={{ minWidth: column.width }}
-      className="status-footer flex"
+      className="number-footer btn"
       onClick={(ev) => {
         ev.preventDefault();
         toggleMenu(toggleMenus, 'numberMenu', group.id);
       }}
-    ></div>
+    >
+      {/* <div className="number">{calcNumbers(column.function)}</div> */}
+      <div className="function">{column.function}</div>
+    </div>
   );
 }
--- src/js/cmps/menus/NumberMenu.jsx
+++ src/js/cmps/menus/NumberMenu.jsx
@@ -1,3 +1,24 @@
 export function NumberMenu() {
-  return <div className=""></div>;
+  return (
+    <div className="menu-modal number-menu">
+      <div className="units">
+        <div className="unit-title title">Unit</div>
+        <div className="unit-list list flex">
+          <div className="symbol">$</div>
+          <div className="symbol selected">%</div>
+          <div className="symbol">₪</div>
+        </div>
+      </div>
+      <div className="functions">
+        <div className="function-title title">Function</div>
+        <div className="function-list list flex">
+          <div className="avg btn flex auto-center">Average</div>
+          <div className="sum btn flex auto-center">Sum</div>
+          <div className="min btn flex auto-center">Min</div>
+          <div className="max btn flex auto-center selected">Max</div>
+          <div className="count btn flex auto-center ">Count</div>
+        </div>
+      </div>
+    </div>
+  );
 }
