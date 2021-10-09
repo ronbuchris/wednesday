619646a663bcc4c95afbd0ff3b69c785d7ad66c6
Adir cohen <adirco123@gmail.com>
2021-10-09 11:58:16 +0300

    adsfdsa

--- src/js/cmps/charts/PersonChart.jsx
+++ src/js/cmps/charts/PersonChart.jsx
@@ -3,12 +3,11 @@ import { Bar } from 'react-chartjs-2';
 export function PersonChart({ personsCount }) {
     const persons = Object.keys(personsCount)
     const numbers = Object.values(personsCount)
-    const colors = ["#9cd326",
-        "#037f4c", "#0086c0", "#579bfc", "#66ccff", "#a25ddc",
-        "#784bd1", "#808080", "#333333", "#ff7575", "#faa1f1",
-        "#4eccc6", "#5559df", "#401694", "#563e3e", "#bda8f9",
-        "#2b76e5", "#a9bee8", "#d974b0", "#9d99b9", "#ad967a",
-        "#a1e3f6", "#bd816e", "#175a63"
+    const colors = ["#0086c0", "#0086c0", "#0086c0", "#0086c0",
+        "#0086c0", "#0086c0", "#0086c0", "#0086c0", "#0086c0",
+        "#0086c0", "#0086c0", "#0086c0", "#0086c0", "#0086c0",
+        "#0086c0", "#0086c0", "#0086c0", "#0086c0", "#0086c0",
+        "#0086c0", "#0086c0", "#0086c0"
     ]
     const data = {
         labels: persons,
@@ -23,6 +22,8 @@ export function PersonChart({ personsCount }) {
         ],
     };
     const options = {
+        type:'horizontal',
+        maintainAspectRatio: false,
         indexAxis: 'y',
         elements: {
             bar: {
