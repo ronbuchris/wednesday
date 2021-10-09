55b51ee5367c5767e6cf2a5e13d0d06a0106beab
Adir cohen <adirco123@gmail.com>
2021-10-09 12:34:35 +0300

    das

--- src/js/cmps/board/DashboardView.jsx
+++ src/js/cmps/board/DashboardView.jsx
@@ -6,6 +6,10 @@ import { StatusChart } from '../charts/StatusChart';
 import { PersonChart } from '../charts/PersonChart';
 import { Loader } from '../Loader';
 import Group from 'monday-ui-react-core/dist/icons/Group';
+import Item from 'monday-ui-react-core/dist/icons/Item';
+import Person from 'monday-ui-react-core/dist/icons/Person';
+import Activity from 'monday-ui-react-core/dist/icons/Activity';
+import Update from 'monday-ui-react-core/dist/icons/Update';
 
 class _DashboardView extends React.Component {
   componentDidMount() {
@@ -17,7 +21,17 @@ class _DashboardView extends React.Component {
   render() {
     const { statuses, personsCount,board } = this.props;
     if (!statuses.length) return <div><Loader/></div>;
-
+    const numbers = Object.values(statuses[0]);
+    var sum = 0
+    numbers.forEach(num => {
+      sum += num
+    })
+    var updateSum = 0
+    board.groups.forEach(group => {
+      group.items.forEach(item => {
+        updateSum += item.updates.length
+      })
+    })
     return (
       <div className="dashboard-preview flex">
         <div className="data-container">
@@ -27,19 +41,24 @@ class _DashboardView extends React.Component {
               <p>Groups</p>
             </div>
             <div className="data-box">
-              <Group/>
+              <Person/>
               <p>{Object.keys(personsCount).length}</p>
               <p>Persons</p>
             </div>
             <div className="data-box">
-              <Group/>
-              <p>{board.groups.length}</p>
-              <p>Groups</p>
+              <Item/>
+              <p>{sum}</p>
+              <p>Items</p>
             </div>
             <div className="data-box">
-              <Group/>
-              <p>{board.groups.length}</p>
-              <p>Groups</p>
+            <Activity/>
+              <p>{board.activities.length}</p>
+            <p>Activities</p>
+            </div>
+            <div className="data-box">
+            <Update/>
+            <p>{updateSum}</p>
+            <p>Updates</p>
             </div>
         </div>
         <div className="charts-container flex column auto-center">
