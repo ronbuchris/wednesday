bff3c74f06f1a544e2d46f602045f74565c252ca
On Chetrit <on7chetrit@gmail.com>
2021-10-09 22:46:11 +0300

    image

--- src/js/cmps/MainHeader.jsx
+++ src/js/cmps/MainHeader.jsx
@@ -1,61 +1,48 @@
-import React from 'react';
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { IoIosArrowDown } from 'react-icons/io';
 import { onLogin } from '../store/actions/user.actions.js';
 import mainlogo from '../../assets/img/logo/mainlogo.png';
 
-class _MainHeader extends React.Component {
-  state = {
-    isActive: false,
-  };
+function _MainHeader() {
+  const [active, setActive] = useState(false);
 
-  onActive = () => {
-    const { isActive } = this.state;
-    this.setState({ isActive: !isActive });
-  };
-
-  render() {
-    const { isActive } = this.state;
-    return (
-      <header className="main-header main-container">
-        <div className="header-content flex space-between align-center">
-          <div className="left flex">
-            <NavLink exact to="/">
-              <img src={mainlogo} alt="logo" className="logo-home" />
-            </NavLink>
-            <div className="nav-btn flex align-center">
-              <span>Product</span>
-              <IoIosArrowDown />
-            </div>
-            <div className="nav-btn flex align-center">
-              <span>Solutions</span>
-              <IoIosArrowDown />
-            </div>
-            <div className="nav-btn">Templates</div>
-            <div className="nav-btn">Pricing</div>
-            {/* <NavLink exact to="/templates">
-            Templates
-          </NavLink> */}
+  return (
+    <header className="main-header main-container">
+      <div className="header-content flex space-between align-center">
+        <div className="left flex">
+          <NavLink exact to="/">
+            <img src={mainlogo} alt="logo" className="logo-home" />
+          </NavLink>
+          <div className="nav-btn flex align-center">
+            <span>Product</span>
+            <IoIosArrowDown />
           </div>
-          <div className="right flex align-center">
-            <NavLink exact to="/login">
-              Log in
-            </NavLink>
-            <div className="started btn">Get Started</div>
+          <div className="nav-btn flex align-center">
+            <span>Solutions</span>
+            <IoIosArrowDown />
           </div>
+          <div className="nav-btn">Templates</div>
+          <div className="nav-btn">Pricing</div>
         </div>
         <div
-          className={`hamburger ${isActive ? 'is-active' : ''}`}
-          onClick={this.onActive}
+          className={`hamburger ${active ? 'is-active' : ''}`}
+          onClick={() => setActive(!active)}
         >
           <span></span>
           <span></span>
           <span></span>
         </div>
-      </header>
-    );
-  }
+        <div className="right flex auto-center">
+          <NavLink exact to="/login">
+            Log in
+          </NavLink>
+          <div className="started btn">Get Started</div>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 const mapDispatchToProps = {
--- src/js/pages/HomePage.jsx
+++ src/js/pages/HomePage.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import NavigationChevronRight from 'monday-ui-react-core/dist/icons/NavigationChevronRight';
+
+import mainlogo from '../../assets/img/logo/mainlogo.png';
+import image1 from '../../assets/img/homepage/image1.jpg';
+import image2 from '../../assets/img/homepage/image2.png';
 
 export class HomePage extends React.Component {
   componentDidMount() {
@@ -7,15 +12,68 @@ export class HomePage extends React.Component {
   render() {
     return (
       <div className="main-home-page ">
-        <div className="home-page flex justify-center">
+        <div className="home-page flex column justify-center">
           <div className="head-line flex column align-center">
             <h1>Work Without Limits</h1>
             <h2>
               Easily build, run, and scale your dream workflows on one platform.
               <br></br>
-              What would you like to manage with monday.com Work OS?
+              What would you like to manage with wednesday Work OS?
             </h2>
           </div>
+          <div className="started flex auto-center btn">
+            Get Started
+            <NavigationChevronRight />
+          </div>
+          <div className="home-section flex">
+            <div className="text">
+              <h2 className="title">Manage everything in one workspace</h2>
+              <div className="info">
+                Planning, tracking and delivering your team’s best work has
+                never been easier
+              </div>
+            </div>
+            <img src={image2} alt="features" />
+          </div>
+          <div className="home-section flex">
+            <div className="text">
+              <h2 className="title">Visualize work with Views</h2>
+              <div className="info">
+                View data as a kanban, dahsboard and more
+              </div>
+            </div>
+            <img src={image1} alt="features" />
+          </div>
+          <div className="home-section flex">
+            <div className="text">
+              <h2 className="title"></h2>
+              <div className="info"></div>
+            </div>
+            {/* <img src={image1} alt="features" /> */}
+          </div>
+
+          <div className="bottom-section">
+            <h2>Try wednesday for your team</h2>
+            <div className="info">
+              14-day free trial | No credit card needed
+            </div>
+          </div>
+          <footer className="flex column">
+            <div className="links flex auto-center">
+              <div className="link">About us</div>
+              <div className="link">Become a partner</div>
+              <div className="link">Careers</div>
+              <div className="link">Blog</div>
+              <div className="link">Pricing</div>
+              <div className="link">Contact sales</div>
+              <div className="link">Terms and privacy</div>
+              <div className="link">Product overview</div>
+            </div>
+            <div className="logo-footer">
+              <img src={mainlogo} alt="logo" />
+            </div>
+            <div className="social"></div>
+          </footer>
         </div>
       </div>
     );
